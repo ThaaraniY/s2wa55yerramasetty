2be4a46e1b0545e81309656116066528c2086de4
ThaaraniY <98137201+ThaaraniY@users.noreply.github.com>
2022-03-30 11:00:08 -0500

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-times=0;
+times= 0;
 
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  times+=1;
-  res.send('User accesses are:',+ times);
+  times= times+1;
+  res.send('User accesses are:'+ times);
 });
 
 module.exports = router;
